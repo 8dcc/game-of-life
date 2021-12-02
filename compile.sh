7bf274d5730e6c42d009bfd6c675d53dcb7602e3
ravi0li <29655971+r4v10l1@users.noreply.github.com>
2021-12-02 16:32:15 +0000

    Compile script

--- compile.sh
+++ compile.sh
@@ -7,15 +7,17 @@ if [[ $(command -v sdl2-config) == "" ]]; then
 fi
 
 SDL_FLAGS=$(sdl2-config --cflags --libs)
+OUTPUT_FILE="game-of-life.o"
 
 if [[ $1 == "" ]]; then
 	echo "No arguments. Please enter a file. Exiting..."
 	exit 1;
 fi
 
-gcc -o output.o $1 $SDL_FLAGS
+# Compile the file
+gcc -o $OUTPUT_FILE $1 $SDL_FLAGS
 
 if [[ $2 == "comp" ]]; then
 	echo "Compiled! Running..."
-	./output.o
+	./$OUTPUT_FILE
 fi
